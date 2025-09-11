d3f11614c1d5849747b09abc64910a973e61aabf
KHJ-MINT <hari0812@naver.com>
2025-09-11 15:17:11 +0900

    git 테스트

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './scss/app.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './components/Main';
 
-
 function App() {
   return (
     <div className="App" id='wrap'>

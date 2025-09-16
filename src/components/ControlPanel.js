90240a85b434d348cd6d4aa85bb56a4e98ed3ab4
KHJ-MINT <hari0812@naver.com>
2025-09-16 16:57:51 +0900

    메인 페이지 스타일 완료

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,16 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSort, faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const ControlPanel = () => {
     return (
         <div className="control-panel-wrap">
             <div className="control-panel-container">
                 <div className="control-btn order-filter">
                     <button type="button" className="order-filter-btn">
-                        <span className="btn-icon">아이콘</span>
+                        <span className="btn-icon"><FontAwesomeIcon icon={faSort} /></span>
                         <span className="btn-txt">정렬 및 필터</span>
                     </button>
                 </div>
                 <div className="control-btn add-ticket">
                     <button type="button" className="add-ticket-btn">
-                        <span className="btn-icon">아이콘</span>
+                        <span className="btn-icon"><FontAwesomeIcon icon={faPlus} /></span>
                         <span className="btn-txt">티켓 추가하기</span>
                     </button>
                 </div>

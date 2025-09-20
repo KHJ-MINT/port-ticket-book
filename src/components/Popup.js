6afdac98ba5e071aaa14029119bd7ac2ad2fe78d
KHJ-MINT <hari0812@naver.com>
2025-09-20 22:16:15 +0900

    팝업의 이전, 다음 버튼 시 내용 변경되는 기능 추가

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faChevronRight, faChevronLeft, faMagnifyingGlass, faStar } from "@fortawesome/free-solid-svg-icons";
 
 const Popup = ({ onClose }) => {
+    //close 버튼을 누르면 input의 모든 내용 삭제(클리어)
+    //search input의 검색 버튼을 누르면 뮤지컬 제목 검색
+    //검색 후 공연장 정보와 출연진 정보 자동 입력, 포스터 이미지를 가져와 자동 출력
+    //1, 2, 3단계 완료 후 티켓을 등록 완료하면 등록 완료 팝업과 함께 티켓 리스트 출력.
+    //로컬 데이터베이스에 입력.
+    //한 페이지에 최대 6개의 티켓 출력. 6개를 넘기면 페이지네이션 추가.
+
+    const [step, setStep] = useState(1); //초기값은 1로
+    const totalStep = 3; //총 3단계
+
+    //다음 버튼 클릭 시
+    const handelNext = () => {
+        setStep(prev => (prev < totalStep ? prev + 1 : prev)); //prev가 totalStep보다 작으면 1씩 증가
+    };
+
+    const handelPrev = () => {
+        setStep(prev => (prev > 1 ? prev - 1 : prev)); //prev가 1보다 크면 1씩 감소
+    }
+
     return (
         <div className="popup-wrap">
             <div className="popup-container">
                 <div className="popup-header">
-                    <span className="current-num">1</span><span className="seperate">/</span><span className='total-num'>3</span>
+                    <span className="current-num">{step}</span><span className="seperate">/</span><span className='total-num'>{totalStep}</span>
                 </div>
                 <div className="close-btn"><button type="button" onClick={onClose}><FontAwesomeIcon icon={faXmark} /></button></div>
                 <div className="popup-content">
                     <form id="popup-form">
-                        <div className="first-content-wrap">
+                        <div className={`first-content-wrap ${step === 1 ? '' : 'close'}`}>
                             <div className="form-input-wrap">
                                 <input type="text" className="form-input" name="perform-title" placeholder='공연 제목을 입력하세요.' />
                                 <button className="popup-search-btn"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
@@ -39,7 +59,7 @@ const Popup = ({ onClose }) => {
                                 </div>
                             </div>
                         </div>
-                        <div className="second-content-wrap close">
+                        <div className={`second-content-wrap ${step === 2 ? '' : 'close'}`}>
                             <div className="review-star-wrap">
                                 <span className='title'>관람 후기</span>
                                 <span className="review-star"><FontAwesomeIcon icon={faStar} /></span>
@@ -52,7 +72,7 @@ const Popup = ({ onClose }) => {
                                 <textarea name="review" className='review-textarea' placeholder='관람 후기를 작성해주세요.'></textarea>
                             </div>
                         </div>
-                        <div className="third-content-wrap close">
+                        <div className={`third-content-wrap ${step === 3 ? '' : 'close'}`}>
                             <h6 className="title">관람 추가 정보</h6>
                             <div className="add-review-info-wrap">
                                 <div className="seat-input-wrap">
@@ -79,8 +99,16 @@ const Popup = ({ onClose }) => {
                 </div>
                 <div className="popup-footer">
                     <div className="popup-btn-wrap">
-                        <button className="prev-btn btn"><FontAwesomeIcon icon={faChevronLeft} />이전</button>
-                        <button className="next-btn btn">다음<FontAwesomeIcon icon={faChevronRight} /></button>
+                        <button className="prev-btn btn"
+                            type='button'
+                            onClick={handelPrev}
+                            disabled={step === 1}
+                        ><FontAwesomeIcon icon={faChevronLeft} />이전</button>
+                        <button className="next-btn btn"
+                            type='button'
+                            onClick={handelNext}
+                            disabled={step === totalStep}
+                        >다음<FontAwesomeIcon icon={faChevronRight} /></button>
                     </div>
                 </div>
             </div>

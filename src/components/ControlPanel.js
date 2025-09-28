6d8a2de2976a879cf4522c49c0dd0fd00729904f
KHJ-MINT <hari0812@naver.com>
2025-09-28 16:38:39 +0900

    티켓 정렬 순서 변경하기 구현

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,15 +1,46 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSort, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
 
-const ControlPanel = ({ onOpen }) => {
+const ControlPanel = ({ onOpen, tickets, displayTickets, setDisplayTickets, sortKey, sortOrder, setSortKey, setSortOrder }) => {
+    const [filterOpen, setFilterOpen] = useState(false); //정렬 버튼 상태 관리
+
+    //정렬 버튼 클릭 시 옵션 보이기/숨기기
+    const handeleToggleFilter = () => {
+        setFilterOpen(open => !open);
+    };
+
+    //정렬 옵션 변경 핸들러
+    const handleSortChange = (key, order) => {
+        setSortKey(key);
+        setSortOrder(order);
+    };
+
     return (
         <div className="control-panel-wrap">
             <div className="control-panel-container">
                 <div className="control-btn order-filter">
-                    <button type="button" className="order-filter-btn">
+                    <button type="button"
+                        className="order-filter-btn"
+                        onClick={handeleToggleFilter}
+                    >
                         <span className="btn-icon"><FontAwesomeIcon icon={faSort} /></span>
-                        <span className="btn-txt">정렬 및 필터</span>
+                        <span className="btn-txt">티켓 정렬 변경</span>
                     </button>
+                    <ul className={`order-filter-lists ${filterOpen ? 'open' : ''}`}>
+                        <li className="order-filter-item selected">
+                            <button type="button" className="order-filter-btn date-desc" onClick={() => handleSortChange('date', 'asc')}>관람일 오름차순</button>
+                        </li>
+                        <li className="order-filter-item">
+                            <button type="button" className="order-filter-btn date-asc" onClick={() => handleSortChange('date', 'desc')}>관람일 내림차순</button>
+                        </li>
+                        <li className="order-filter-item">
+                            <button type="button" className="order-filter-btn title-desc" onClick={() => handleSortChange('title', 'asc')}>제목 오름차순</button>
+                        </li>
+                        <li className="order-filter-item">
+                            <button type="button" className="order-filter-btn title-asc" onClick={() => handleSortChange('title', 'desc')}>제목 내림차순</button>
+                        </li>
+                    </ul>
                 </div>
                 <div className="control-btn add-ticket">
                     <button type="button" className="add-ticket-btn" onClick={onOpen}>
--- src/components/TicketGrid.js
+++ src/components/TicketGrid.js
@@ -1,23 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ControlPanel from "./ControlPanel";
 import TicketList from "./TicketList";
 import TicketDetail from "./TicketDetail";
 
+const sortTickets = (tickets, key, order = 'asc') => {
+    const sortedTickets = [...tickets]; //원본 배열 훼손 방지
+
+    sortedTickets.sort((a, b) => {
+        //정렬 기준
+        const valA = a[key];
+        const valB = b[key];
+
+        let comparison = 0;
+
+        if (key === 'date') {
+            //날짜 정렬
+            comparison = valA.localeCompare(valB);
+        } else if (key === 'title') {
+            //문자열 정렬
+            comparison = valA.localeCompare(valB, 'ko', { sensitivity: 'base' });
+        }
+
+        return order === 'desc' ? comparison * -1 : comparison;
+    });
+
+    return sortedTickets;
+}
+
 const TicketGrid = ({ tickets, onAddTicket, setTickets }) => {
-    const [selectedTicketId, setSelectedTicketId] = useState(null);
+    const [selectedTicketId, setSelectedTicketId] = useState(null); //선택한 티켓 아이디
+    const [sortKey, setSortKey] = useState('date'); //정렬 기준. 기본은 날짜.
+    const [sortOrder, setSortOrder] = useState('asc'); //정렬 순서. 기본은 오름차순.
+    const [displayTickets, setDisplayTickets] = useState(() => sortTickets(tickets, sortKey, sortOrder)); //정렬된 결과를 저장
+
+    //날짜 오름차순 정렬 함수
+    // const sortTicketAsc = (tickets) => {
+    //     return [...tickets].sort((a, b) => a.date.localeCompare(b.date));
+    // }
 
     //선택한 티켓 아이디
     const selectedTicket = tickets.find(ticket => ticket.id === selectedTicketId);
+
+    //티켓 변경 시에도 날짜 오름차순 정렬
+    useEffect(() => {
+        setDisplayTickets(sortTickets(tickets, sortKey, sortOrder));
+    }, [tickets, sortKey, sortOrder]); // tickets가 바뀔 때만 오름차순으로 초기화
 
     return (
         <div className="right-container">
 
             {selectedTicketId
-                ? <TicketDetail ticket={selectedTicket} onBack={() => setSelectedTicketId(null)} setTickets={setTickets} />
+                ? <TicketDetail
+                    ticket={selectedTicket}
+                    onBack={() => setSelectedTicketId(null)}
+                    setTickets={setTickets}
+                />
                 :
                 <>
-                    <ControlPanel onOpen={onAddTicket} />
-                    <TicketList tickets={tickets} onSelect={setSelectedTicketId} />
+                    <ControlPanel
+                        onOpen={onAddTicket}
+                        sortKey={sortKey}
+                        sortOrder={sortOrder}
+                        setSortKey={setSortKey}
+                        setSortOrder={setSortOrder}
+                        tickets={tickets}
+                        displayTickets={displayTickets}
+                        setDisplayTickets={setDisplayTickets}
+                    />
+                    <TicketList tickets={displayTickets} onSelect={setSelectedTicketId} />
                 </>
             }
 
